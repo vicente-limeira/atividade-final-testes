507faaf238e7d9df6aa614e07bb8997bdd676147
vicente-limeira <vicentelimeira@hotmail.com>
2022-10-03 17:02:15 -0300

    Versão final

--- src/test/java/com/example/requisicaoemprestimo/integracao/RequisicaoEmprestimoUseCaseTests.java
+++ src/test/java/com/example/requisicaoemprestimo/integracao/RequisicaoEmprestimoUseCaseTests.java
@@ -1,25 +1,80 @@
 package com.example.requisicaoemprestimo.integracao;
 
+import com.example.requisicaoemprestimo.domain.models.Emprestimo;
+import com.example.requisicaoemprestimo.domain.models.ResultadoAnalise;
+import com.example.requisicaoemprestimo.domain.models.ResultadoTesouraria;
+import com.example.requisicaoemprestimo.domain.ports.IAnaliseProxy;
+import com.example.requisicaoemprestimo.domain.ports.ITesourariaProxy;
+import com.example.requisicaoemprestimo.domain.usecases.RequisicaoEmprestimoUseCase;
 import org.junit.jupiter.api.Test;
+
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 //TODO: Use o mokito para realizar testes TOP-DOWN para criar dublês das interfaces IAnaliseProxy e ITesourariaProxy
 public class RequisicaoEmprestimoUseCaseTests {
 
     //TODO: Setup das classes Mocks e Instância real da classe RequisicaoEmprestimoUseCase
+    IAnaliseProxy iAnaliseProxy = mock(IAnaliseProxy.class);
+    ITesourariaProxy iTesourariaProxy = mock(ITesourariaProxy.class);
+    RequisicaoEmprestimoUseCase requisicaoEmprestimoUseCase = new RequisicaoEmprestimoUseCase(iAnaliseProxy, iTesourariaProxy);
 
+    final String approved = "APROVADO", disapproved = "REPROVADO";
 
     @Test
-    public void test1(){
+    public void test_aprovado(){
         //TODO Fazer um teste caminho Feliz (TUDO FUNCIONA BEM)
+        Emprestimo emprestimo;
+
+        ResultadoAnalise resultadoAnalise = new ResultadoAnalise(true, new String[] {approved});
+        when(iAnaliseProxy.solicitarAnaliseDeCredito(any())).thenReturn(resultadoAnalise);
+
+        ResultadoTesouraria resultadoTesouraria = new ResultadoTesouraria(true, approved);
+        when(iTesourariaProxy.solicitarLiberacaoDaTesouraria(any())).thenReturn(resultadoTesouraria);
+
+        assertAll(() -> requisicaoEmprestimoUseCase.executar(UUID.randomUUID(), 100.00, 2));
+        emprestimo = requisicaoEmprestimoUseCase.executar(UUID.randomUUID(), 100.00, 2);
+        assertEquals(true, emprestimo.isEmprestimoFoiAprovado());
     }
 
     @Test
-    public void test2(){
+    public void test_analise_reprovado(){
         //TODO Fazer um teste caminho INFELIZ IAnaliseProxy retornando uma Análise reprovada
+        Emprestimo emprestimo;
+
+        ResultadoAnalise analise = new ResultadoAnalise(false, new String[] {disapproved});
+        when(iAnaliseProxy.solicitarAnaliseDeCredito(any())).thenReturn(analise);
+
+        ResultadoTesouraria tesouraria = new ResultadoTesouraria(true, disapproved);
+        when(iTesourariaProxy.solicitarLiberacaoDaTesouraria(any())).thenReturn(tesouraria);
+
+        assertAll(() -> requisicaoEmprestimoUseCase.executar(UUID.randomUUID(), 1000.00, 10));
+        emprestimo = requisicaoEmprestimoUseCase.executar(UUID.randomUUID(), 1000.00, 10);
+
+        assertEquals(analise, emprestimo.getResultadoAnalise());
+        assertEquals(false, emprestimo.isEmprestimoFoiAprovado());
     }
 
     @Test
-    public void test3(){
+    public void test_tesouraria_reprovado(){
         //TODO Fazer um teste caminho INFELIZ ITesourariaProxy retornando resultado reprovado
+        Emprestimo emprestimo;
+
+        ResultadoAnalise resultadoAnalise = new ResultadoAnalise(true, new String[] {approved});
+        when(iAnaliseProxy.solicitarAnaliseDeCredito(any())).thenReturn(resultadoAnalise);
+
+        ResultadoTesouraria resultadoTesouraria = new ResultadoTesouraria(false, disapproved);
+        when(iTesourariaProxy.solicitarLiberacaoDaTesouraria(any())).thenReturn(resultadoTesouraria);
+
+        assertAll(() -> requisicaoEmprestimoUseCase.executar(UUID.randomUUID(), 1000.00, 10));
+        emprestimo = requisicaoEmprestimoUseCase.executar(UUID.randomUUID(), 1000.00, 10);
+
+        assertEquals(resultadoTesouraria, emprestimo.getResultadoTesouraria());
+        assertEquals(false, emprestimo.isEmprestimoFoiAprovado());
     }
 }
--- src/test/java/com/example/requisicaoemprestimo/unitarios/EmprestimoTests.java
+++ src/test/java/com/example/requisicaoemprestimo/unitarios/EmprestimoTests.java
@@ -1,9 +1,14 @@
 package com.example.requisicaoemprestimo.unitarios;
 
+import com.example.requisicaoemprestimo.domain.models.Emprestimo;
+import com.example.requisicaoemprestimo.domain.models.Parcela;
+import com.example.requisicaoemprestimo.domain.models.ResultadoAnalise;
+import com.example.requisicaoemprestimo.domain.models.ResultadoTesouraria;
 import org.junit.jupiter.api.Test;
 
-import java.text.DecimalFormat;
-import java.util.UUID;
+import java.util.Optional;
+
+import static com.example.requisicaoemprestimo.unitarios.EmprestimoTestsFixture.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class EmprestimoTests {
@@ -12,14 +17,19 @@ public class EmprestimoTests {
         // ARRANGE
         //TODO: FAÇA USO DO FIXTURE
         //TODO: Crie uma classe Emprestimo com valor 100 e quantidade de parcelas 12
+        Emprestimo emprestimo = emprestimoAprovado(100, 12);
 
         // ACT
         //TODO: Recupere as parcelas do emprestimo
+        Optional<Parcela[]> parcelas = emprestimo.getParcelas();
 
         // ASSERTS
         //TODO: Validar se existe parcelas
+        assertNotNull(parcelas);
         //TODO: Validar o valor total de empréstimo é 106.50
+        assertEquals(106.50, emprestimo.getValorTotalEmprestimo());
         //TODO: Validar se o número de parcelas é 12
+        assertEquals(12, emprestimo.getQuantidadeParcelasSolicitadas());
     }
 
     @Test
@@ -27,13 +37,19 @@ public class EmprestimoTests {
         // ARRANGE
         //TODO: FAÇA USO DO FIXTURE
         //TODO: Crie uma classe Emprestimo com valor 100 e quantidade de parcelas 12
+        Emprestimo emprestimo = emprestimoAprovado(100, 12);
 
         // ACT
         //TODO: Crie uma análise de crédito rejeitando a proposta
+        String[] resposta = {"REPROVADO"};
+        ResultadoAnalise resultadoAnalise = buildResultadoAnalise(false, resposta);
+
         //TODO: Associe or resulado ao emprestimo
+        emprestimo.setResultadoAnalise(resultadoAnalise);
 
         // ASSERTS
         //TODO: Validar que o  emprestimo NÃO está aprovado
+        assertTrue(!emprestimo.isEmprestimoFoiAprovado());
     }
 
     @Test
@@ -41,13 +57,17 @@ public class EmprestimoTests {
         // ARRANGE
         //TODO: FAÇA USO DO FIXTURE
         //TODO: Crie uma classe Emprestimo com valor 100 e quantidade de parcelas 12
+        Emprestimo emprestimo = emprestimoAprovado(100, 12);
 
         // ACT
         //TODO: Crie uma solicitação para tesouraria rejeitando a proposta
+        ResultadoTesouraria resultadoTesouraria = buildResultadoTesouraria(false, "INADIMPLENTE", emprestimo);
         //TODO: Associe o resultado ao emprestimo
+        emprestimo.setResultadoTesouraria(resultadoTesouraria);
 
         // ASSERTS
         //TODO: Validar que o  emprestimo NÃO está aprovado
+        assertTrue(!emprestimo.isEmprestimoFoiAprovado());
     }
 
 }
